057bd0155747950ab5875ae6dc291459c7a60ef6
Chris Lee <crlee3@uw.edu>
2020-02-29 21:11:25 -0800

    fix lint errors by using stylr and add rewrite slightly for clarity

--- expand_targets.r
+++ expand_targets.r
@@ -2,11 +2,25 @@ library(dplyr)
 library(tidyr)
 library(jsonlite)
 
-expand_targets <- function(df) { #ex. expand(sample_of_df)
-  l <- lapply(df[, "targets"], function(x) tryCatch({fromJSON(x)}, error = function(e) NA))
-  l <- lapply(l, function(x) tryCatch({spread(x, key = target, value = segment)}, 
-                                      error = function(e) data.frame()))
+expand_targets <- function(df) { # ex. expand(sample_of_df)
+  l <- lapply(df[, "targets"], function(x) {
+    tryCatch(
+      {
+        fromJSON(x)
+      },
+      error = function(e) NA
+    )
+  })
+  l <- lapply(l, function(x) {
+    tryCatch(
+      {
+        spread(x, key = target, value = segment)
+      },
+      error = function(e) data.frame()
+    )
+  })
   names(l) <- df$id
   df_to_join <- bind_rows(l2, .id = "id")
   df <- full_join(df, df_to_join)
+  return(df)
 }
