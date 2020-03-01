20a5cdd90c5fad7c081333837953b832662128d1
Chris Lee <crlee3@uw.edu>
2020-02-29 17:15:33 -0800

    utility function for the political ads by facebook data set: parses json for targets category and flattens it so it is easier to work with

--- expand_targets.r
+++ expand_targets.r
@@ -2,7 +2,7 @@ library(dplyr)
 library(tidyr)
 library(jsonlite)
 
-expand_targets <- function(df) { #ex. expand(targets, sample)
+expand_targets <- function(df) { #ex. expand(sample_of_df)
   l <- lapply(df[, "targets"], function(x) tryCatch({fromJSON(x)}, error = function(e) NA))
   l <- lapply(l, function(x) tryCatch({spread(x, key = target, value = segment)}, 
                                       error = function(e) data.frame()))
