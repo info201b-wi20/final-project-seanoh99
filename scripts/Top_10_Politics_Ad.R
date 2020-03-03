bc1e83602dfcfd90010572c18a599dae1f04e79a
michas23 <59853950+michas23@users.noreply.github.com>
2020-03-02 20:50:53 -0800

    Flipped x-axis labels for legibility

--- scripts/Top_10_Politics_Ad.R
+++ scripts/Top_10_Politics_Ad.R
@@ -7,9 +7,10 @@ library(ggplot2)
 # On the other hand, Beto O'Rourke (democratic) is the first among politicians
 
 Top_10_Politics_Ad <- function(df) {
-  top10 <- df%>%group_by(title) %>% summarize(count =n()) %>% arrange(-count) %>% slice(1:10)
+  top10 <- df%>%group_by(title) %>% summarize(count = n()) %>% arrange(-count) %>% slice(1:10)
   ggplot(data=top10, aes(x=reorder(title, -count), y=count)) +
     geom_bar(width = 0.5, stat = "identity")  +
     ggtitle("Top 10 Political Advertisment on Facebook") +
-    xlab("Figure") + ylab("Count of Ads") +  theme(legend.position = "topright") + theme_classic()
+    xlab("Figure") + ylab("Count of Ads") +  theme(legend.position = "topright") + theme_classic() +
+    theme(axis.text.x = element_text(angle = 60, hjust = 1))
 }
