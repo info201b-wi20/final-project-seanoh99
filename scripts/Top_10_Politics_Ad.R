39fe8e9d7136d02c24932db1676403424316064e
michas23 <59853950+michas23@users.noreply.github.com>
2020-03-02 22:43:39 -0800

    Linted

--- scripts/Top_10_Politics_Ad.R
+++ scripts/Top_10_Politics_Ad.R
@@ -1,16 +1,22 @@
 library(dplyr)
 library(ggplot2)
+library(lintr)
 # This shows the top 10 political ads on facebook.
 # This has a strong correlation to have much actualy money was spent
 # on advertising. (More money spent = higher chance of getting collected)
-# Donald Trump was only the 10th in the number of ads collected. 
+# Donald Trump was only the 10th in the number of ads collected.
 # On the other hand, Beto O'Rourke (democratic) is the first among politicians
 
 Top_10_Politics_Ad <- function(df) {
-  top10 <- df%>%group_by(title) %>% summarize(count = n()) %>% arrange(-count) %>% slice(1:10)
-  ggplot(data=top10, aes(x=reorder(title, -count), y=count)) +
+  top10 <- df %>%
+    group_by(title) %>%
+    summarize(count = n()) %>%
+    arrange(-count) %>%
+    slice(1:10)
+  ggplot(data = top10, aes(x = reorder(title, -count), y = count)) +
     geom_bar(width = 0.5, stat = "identity")  +
     ggtitle("Top 10 Political Advertisment on Facebook") +
-    xlab("Name of Funder") + ylab("Count of Ads") +  theme(legend.position = "topright") + theme_classic() +
+    xlab("Name of Funder") + ylab("Count of Ads") +
+    theme(legend.position = "topright") + theme_classic() +
     theme(axis.text.x = element_text(angle = 60, hjust = 1))
 }
