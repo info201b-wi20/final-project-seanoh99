32373c003de9c894d03bfc1c70a4e9780d702269
michas23 <59853950+michas23@users.noreply.github.com>
2020-03-02 22:50:30 -0800

    Linted

--- scripts/get_summary_info.r
+++ scripts/get_summary_info.r
@@ -3,21 +3,21 @@ library(dplyr)
 ## requires entities df
 get_summary_info <- function(df) {
   observations <- nrow(df)
-  
+
   biggest_advertiser <- df %>%
     filter(advertiser != "") %>%
     group_by(advertiser) %>%
     summarise(total = n()) %>%
     filter(total == max(total)) %>%
     pull(advertiser)
-  
+
   biggest_funder <- df %>%
     filter(paid_for_by != "") %>%
     group_by(paid_for_by) %>%
     summarise(total = n()) %>%
     filter(total == max(total)) %>%
     pull(paid_for_by)
-
+  
   political_count <- df %>%
     filter(!is.na(political)) %>%
     nrow()
@@ -29,22 +29,10 @@ get_summary_info <- function(df) {
       observations = observations,
       advertiser = biggest_advertiser,
       funder = biggest_funder,
-      political_percentage = political_count / (political_count + non_political_count) * 100
+      political_percentage = political_count /
+        (political_count + non_political_count) * 100
     )
   )
 }
 
-## Code is problematic, I'll fix later -Michael
-## most_advertised_org <- df %>%
-##  filter(!is.na(Organization)) %>%
-##  group_by(Organization) %>%
-##  summarise(total = n()) %>%
-##  filter(total == max(total)) %>%
-##  pull(Organization)
-
-##most_advertised_person <- df %>%
-##  filter(!is.na(Person)) %>%
-##  group_by(Person) %>%
-##  summarise(total = n()) %>%
-##  filter(total == max(total)) %>%
-##  pull(Person)
+## Removed code was problematic, I'll fix later -Michael
