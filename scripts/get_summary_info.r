d80488f77fca20f8939bebf320c2df75f1d05d9e
michas23 <59853950+michas23@users.noreply.github.com>
2020-03-02 20:04:59 -0800

    Fixed bugs
    Fixed bugs and commented out two functions that I'm still fixing

--- scripts/get_summary_info.r
+++ scripts/get_summary_info.r
@@ -3,42 +3,48 @@ library(dplyr)
 ## requires entities df
 get_summary_info <- function(df) {
   observations <- nrow(df)
-
-  biggest_advertiser <- sample %>%
+  
+  biggest_advertiser <- df %>%
     filter(advertiser != "") %>%
     group_by(advertiser) %>%
     summarise(total = n()) %>%
     filter(total == max(total)) %>%
     pull(advertiser)
-
-  biggest_funder <- sample %>%
+  
+  biggest_funder <- df %>%
     filter(paid_for_by != "") %>%
     group_by(paid_for_by) %>%
     summarise(total = n()) %>%
     filter(total == max(total)) %>%
     pull(paid_for_by)
 
-  most_advertised_org <- sample %>%
-    filter(!is.na(Organization)) %>%
-    group_by(Organization) %>%
-    summarise(total = n()) %>%
-    filter(total == max(total)) %>%
-    pull(Organization)
-
-  most_advertised_person <- sample %>%
-    filter(!is.na(Person)) %>%
-    group_by(Person) %>%
-    summarise(total = n()) %>%
-    filter(total == max(total)) %>%
-    pull(Person)
-
+  political_count <- df %>%
+    filter(!is.na(political)) %>%
+    nrow()
+  non_political_count <- df %>%
+    filter(!is.na(not_political)) %>%
+    nrow()
   return(
     list(
       observations = observations,
       advertiser = biggest_advertiser,
       funder = biggest_funder,
-      advertised_org = most_advertised_org,
-      advertised_person = most_advertised_person
+      political_percentage = political_count / (political_count + non_political_count) * 100
     )
   )
 }
+
+## Code is problematic, I'll fix later -Michael
+## most_advertised_org <- df %>%
+##  filter(!is.na(Organization)) %>%
+##  group_by(Organization) %>%
+##  summarise(total = n()) %>%
+##  filter(total == max(total)) %>%
+##  pull(Organization)
+
+##most_advertised_person <- df %>%
+##  filter(!is.na(Person)) %>%
+##  group_by(Person) %>%
+##  summarise(total = n()) %>%
+##  filter(total == max(total)) %>%
+##  pull(Person)
